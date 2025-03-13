af51c5c44fdaf0d892ccad4230ee04537fa71cd0
JIAMING ZHANG <zjm100100@163.com>
2025-03-13 20:15:17 +0000

    Add UltrasonicSensor
    Add UltrasonicSensor hpp file

---
+++ UltrasonicSensor.hpp
@@ -0,0 +1,32 @@
+#ifndef ULTRASONIC_SENSOR_HPP
+#define ULTRASONIC_SENSOR_HPP
+
+#include <atomic>
+#include <mutex>
+#include <gpiod.h>
+
+class UltrasonicSensor {
+public:
+    UltrasonicSensor(int trig_pin, int echo_pin);
+    ~UltrasonicSensor();
+    
+    float get_distance();
+    static void signal_handler(int signal);
+    static std::atomic<bool> running;
+
+private:
+    const int trig_pin_;
+    const int echo_pin_;
+    gpiod_chip* chip_ = nullptr;
+    gpiod_line* trig_line_ = nullptr;
+    gpiod_line* echo_line_ = nullptr;
+    std::mutex gpio_mutex_;
+
+    void initialize_gpio();
+    void trigger_pulse() const;
+    float measure_pulse();
+    void cleanup_gpio();
+    void reset_gpio();
+};
+
+#endif 
